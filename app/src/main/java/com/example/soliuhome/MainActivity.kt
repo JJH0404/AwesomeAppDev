e35d0b000685f836d86a25df40d90a04fdedfb1e
JongHyun Jung <hyun831@gmail.com>
2024-01-28 23:08:59 -0600

    test

--- app/src/main/java/com/example/soliuhome/MainActivity.kt
+++ app/src/main/java/com/example/soliuhome/MainActivity.kt
@@ -10,7 +10,7 @@ import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
-yoon
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
